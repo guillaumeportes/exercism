d672261ba77f15e2d8069b6b94b2b598997c02bb
Guillaume Portes <gportes@anotherplaceproductions.com>
2018-09-04 13:20:16 +0100

    initial commit

---
+++ bash/grains/grains.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+# 2^63 is too large to use bash arithmetics so we use bc
+square()
+{
+    ( (($1 < 1)) || (($1 > 64)) ) && echo "Error: invalid input" && exit 1
+    echo "2^($1-1)" | bc
+}
+
+# First implementation was using the above square function, which was very slow.
+# For performance we actually send the whole operation as a string to bc
+total()
+{
+    result="0"
+    for i in $(seq 1 64);
+    do
+        result+="+2^($i-1)"
+    done
+    echo "$result" | bc
+}
+
+if [ "$1" = "total" ]
+then
+    total "$@"
+else
+    square "$@"
+fi
